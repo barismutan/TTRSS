2d8522a96ea595d009c405dd93c8676b38bfdafb
barismutan <barismutan@gmail.com>
2023-07-11 09:14:06 +0200

    before error handling rework

--- ttrss.py
+++ ttrss.py
@@ -28,7 +28,7 @@ class TTRSS:
         self.gpt_config=config['gpt_config']
         # self.mrkdwn_template=config['mrkdwn_template']
         self.zapier_webhook=config['zapier_webhook']
-        self.message_time=config['message_time']
+        # self.message_time=config['message_time']
 
         self.UNREAD_BODY={
             "op":"getHeadlines",
@@ -69,6 +69,8 @@ class TTRSS:
         self.session_id=self.login()
         openai.api_key=self.GPT_API_KEY
 
+        self.extract_link_callbacks=[self.get_read_more_href,self.get_last_body_href]
+        # self.scoring_metric=config['scoring_metric']
 
     def trim_text(self,text):
         #return first 80% of text
@@ -99,16 +101,43 @@ class TTRSS:
         return article['link']
 
     def get_article_link_original(self,data_breaches_link):
+
         response=self.make_request_with_session(data_breaches_link)
+        if response==None:
+            raise NoLinksFoundException() #TODO: change this to a different exception
         html=response.content.decode('utf-8') if response.status_code==200 \
                                             else self.invoke_selenium(data_breaches_link)
         
+        for callback in self.extract_link_callbacks:
+            href=callback(html)
+            if href is not None:
+                print("HREF:"+href) 
+                return href
+            
+        raise NoLinksFoundException()
+
+    def get_last_body_href(self,html):
+        soup = BeautifulSoup(html, 'html.parser')
+        #get the div with the class "entry-content"
+        entry_content=soup.find('div',attrs={'class':'entry-content'})
+        #remove the div with class "crp_related" in the div
+        links = entry_content.select('a:not(div.crp_related a)')
+        print("LINKS:"+str(links))
+
+        if len(links)>0:
+            return links[-1]['href']
+        return None
+    
+        #NOTE: this can probably throw some exception, but I'm not sure what it would be.
+
+
+    def get_read_more_href(self,html):
         pattern=r'Read more at.*?href="(.*?)"'
         href=re.findall(pattern,html,re.DOTALL)
         if len(href)==0:
-            raise NoLinksFoundException()
+            return None
         else:
-            return href[0] #TODO:rework this
+            return href[0]
 
     def remove_excess_whitespace(self,text):
         text=re.sub(r'\n',' ',text)
@@ -137,13 +166,15 @@ class TTRSS:
         # print("ARTICLE:"+article)
         # print(openai.api_key)
         # return
-        completion = oresponse=openai.ChatCompletion.create(
+        logging.debug("Querying GPT-3.5 Turbo.")
+        completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
 
             {"role": "assistant", "content": prompt}
 
-        ]
+        ],
+    timeout=30
     )
         print("Completion:\n"+str(completion))
         try:
@@ -156,10 +187,17 @@ class TTRSS:
         return completion_dict
 
     def make_request_with_session(self,url):
+        try:
+            session=requests.Session()
 
-        session=requests.Session()
-        response=session.get(url,headers=self.EXTERNAL_HEADERS)
-               
+            response=session.get(url,headers=self.EXTERNAL_HEADERS,timeout=10)
+            print("MAKE REQUEST WITH SESSION RESPONSE:"+str(response))
+        except requests.exceptions.MissingSchema as e:
+            logging.error("Missing schema:"+str(e))
+            return None
+        except requests.exceptions.ReadTimeout as e:
+            logging.error("Read timeout:"+str(e))
+            return None
         # print("Response:"+str(response))
         return response
 
@@ -180,37 +218,56 @@ class TTRSS:
                                             ))
 
         return response
+    
+    def mark_as_unread(self,article_id):
+        mark_as_unread_body=self.MARK_AS_UNREAD_BODY
+        mark_as_unread_body['article_ids']=article_id
+        mark_as_unread_body['sid']=self.session_id
+
+        response=requests.post(self.endpoint,data=json.dumps(
+            mark_as_unread_body
+                                            ))
+
+        return response
 
     def process_unread(self,article_id):
-
-        article=self.get_article(article_id)
-        article_link=self.get_article_link_databreaches(article)
-        original_link=self.get_article_link_original(article_link)
-        html=self.make_request_with_session(original_link)
-        text=self.extract_text(html)
-        print("TEXT:"+text)
         try:
-            query_result=self.gpt_query(text)
-            if query_result is None:
-                return None # to not mark as read
-            self.mark_as_read(article_id)
-            print("MARKED AS READ")
-        except openai.error.InvalidRequestError:
+            article=self.get_article(article_id)
+            article_link=self.get_article_link_databreaches(article)
+            original_link=self.get_article_link_original(article_link)
+            print("does it get here?" + original_link)
+            html=self.make_request_with_session(original_link)
+            if html==None:
+                return
+            text=self.extract_text(html)
+            print("TEXT:"+text)
+            try:
+                query_result=self.gpt_query(text)
+                if query_result is None:
+                    return None # to not mark as read
+                self.mark_as_read(article_id)
+                print("MARKED AS READ")
+            except openai.error.InvalidRequestError:
 
-            while True:
-                text=self.trim_text(text)
-                try:
-                    print("Trying again with shorter text")
-                    query_result=self.gpt_query(text)
-                    if query_result is None: 
+                while True:
+                    text=self.trim_text(text)
+                    try:
+                        print("Trying again with shorter text")
+                        query_result=self.gpt_query(text)
+                        if query_result is None: 
+                            # break
+                            return # to not mark as read NEW
+                        self.mark_as_read(article_id)
+                        print("MARKED AS READ")
                         break
-                    self.mark_as_read(article_id)
-                    print("MARKED AS READ")
-                    break
-                except openai.error.InvalidRequestError as e:
-                    print(len(text))
-                    print(e)
-                    continue
+                    except openai.error.InvalidRequestError as e:
+                        print(len(text))
+                        print(e)
+                        continue
+        except Exception as e:
+            logging.error(e)
+            # self.mark_as_unread(article_id)#TODO
+            return None # to not mark as read
 
         
         # print("QUERY RESULT:" +query_result)
@@ -255,6 +312,7 @@ class TTRSS:
         batch=[]
         for headline in headlines:
             try:
+                print("HEADLINE and ID:"+str(headline)+str(headline['id']))
                 query_result=self.process_unread(headline['id'])
                 if query_result is None: #way better idea is to wrap the whole thing in a try except for SyntaxError.
                     continue
@@ -279,20 +337,23 @@ class TTRSS:
 
 def schedule_job(config):
     ttrss=TTRSS(config)
-    for time in config['times']:
-        schedule.every().day.at(time).do(ttrss.job)
+    for message_time in config['message_times']:
+        schedule.every().day.at(message_time).do(ttrss.job)
 
     while True:
         schedule.run_pending()
         #sleep for 11 hours and 59 minutes
         time.sleep(43140)
 
+def score_article(self,gpt_response):
+    pass
 
 if __name__ =="__main__":
     logging.basicConfig(filename='TTRSS.log',level=logging.DEBUG)
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--config', dest='config', default='config.json',
                         help='config file path', required=False)
+    
     
     args = parser.parse_args()
     with open(args.config) as f:
@@ -301,7 +362,7 @@ if __name__ =="__main__":
     ttrss=TTRSS(config)
     ttrss.job()
 
-
+    logging.info("[{}]Starting TTRSS".format(str(datetime.now())))
     # schedule_job(config) #NOTE: uncomment this to run on a schedule
 
 
@@ -312,7 +373,13 @@ if __name__ =="__main__":
 #NOTE: is there a limit on the size of the text we can send to slack?
 #NOTE: i think gpt just outputs 'Not specified.' as the whole text if it can't find anything useful, do we
 #catch the exception and just skip the article, or do we change the prompt?
-  
+#NOTE: sometimes the request takes too long, modify the default timeout for requests.
+#NOTE: sometimes when the page has a banner, we get stuck.
+#NOTE: sometimes the page has a paywall, we get stuck.
+#NOTE: we need to add a timeout to gpt query.
+#NOTE: 502 error from bad gateway when querying gpt, need to handle this.
+#NOTE: we get SSL error from some sites, need to handle this.
+#NOTE: better idea: DO THE MARK AS READ AFTER THE CALL TO THE PROCESS UNREAD FUNCTION, embed some logic there.
             
     
 
